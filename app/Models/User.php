353316d48f98e21e308e4aa52c0ea117440bb124
Rettorio <ardiansyahrukua07@gmail.com>
2025-06-25 23:21:42 +0900

    tambahkan role pada tabel user

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'role',
         'password',
     ];
 
@@ -41,4 +42,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    static $ROLE_ADMIN = "admin";
+    static $ROLE_CONSUMER = "admin";
+    static $ROLE_PENYELENGGARA = "admin";
 }
