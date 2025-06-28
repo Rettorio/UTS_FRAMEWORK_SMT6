e7ba4d5eab8bd5043f725130c8106e0ec1d139e2
Rettorio <ardiansyahrukua07@gmail.com>
2025-06-28 16:18:56 +0900

    tambah logout btn

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -16,3 +16,14 @@
         <span class="menu-title">Daftar Event</span>
       </a>
     </li>
+    <li class="nav-item">
+      <!-- <div class="nav-link"> -->
+        <form method="POST" action="{{ route('logout') }}">
+          @csrf
+            <button class="nav-link" type="submit">
+                  <i class="mdi mdi-logout menu-icon"></i>
+                  <span class="menu-title">Logout</span>
+            </button>
+        </form>
+      <!-- </div> -->
+    </li>
